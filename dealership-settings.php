a99509182c6c51b85ce3d8264a92d0251b28913b
kevinjmcmahonjr <kevinjmcmahonjr@gmail.com>
2018-10-05 06:30:57 -0400

    Update dealership-settings.php

--- dealership-settings.php
+++ dealership-settings.php
@@ -20,6 +20,7 @@ function motent_dealership_options_page( $settings_pages ) {
         'id'          => 'dealership-settings',
         'option_name' => 'dealership_settings',
         'menu_title'  => 'Dealership Details',
+	'columns' => '1',
 	'style' => 'no-boxes',
 	'position'    => '22'
     );
