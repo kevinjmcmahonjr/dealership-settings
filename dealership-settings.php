3fd009bcf976c2a8439d20688a6fb532f2f99687
kevinjmcmahonjr <kevinjmcmahonjr@gmail.com>
2018-10-04 22:28:02 -0400

    Update dealership-settings.php

--- dealership-settings.php
+++ dealership-settings.php
@@ -14,11 +14,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-add_filter( 'mb_settings_pages', 'prefix_options_page' );
-function prefix_options_page( $settings_pages ) {
+add_filter( 'mb_settings_pages', 'motent_dealership_options_page' );
+function motent_dealership_options_page( $settings_pages ) {
     $settings_pages[] = array(
         'id'          => 'dealership-settings',
-        'option_name' => 'dealership-settings',
+        'option_name' => 'dealership_settings',
         'menu_title'  => 'Dealership Details',
     );
     return $settings_pages;
