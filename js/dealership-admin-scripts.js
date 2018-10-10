d586b03f187416a2e1a7390898346915d763a5dd
kevinjmcmahonjr <kevinjmcmahonjr@gmail.com>
2018-10-10 14:16:50 -0400

    Update dealership-admin-scripts.js

--- js/dealership-admin-scripts.js
+++ js/dealership-admin-scripts.js
@@ -1,4 +1,6 @@
-var vinInputField  = new Cleave('.vin-input-field > .rwmb-input > input.rwmb-text', {
-    blocks: [17],
-    uppercase: true
+document.addEventListener('DOMContentLoaded', () => {
+    var vinInputField  = new Cleave('.vin-input-field > .rwmb-input > input.rwmb-text', {
+        blocks: [17],
+        uppercase: true
+    });
 });
