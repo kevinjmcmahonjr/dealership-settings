d186e519f4d9c40d59481e4ad99e4bbc78c328b9
kevinjmcmahonjr <kevinjmcmahonjr@gmail.com>
2018-10-10 16:46:45 -0400

    Update dealership-admin-scripts.js

--- js/dealership-admin-scripts.js
+++ js/dealership-admin-scripts.js
@@ -5,13 +5,27 @@ function setAllInputCleaves(){
     });
 }
 
+function vinDecodeMakeRequest(vinNumber){
+    var vinDecodeBaseURL = 'https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/';
+    var vinDecodeResponseFormat = '?format=json';
+    var vinDecodeCallback = '&jsoncallback=processVinDecodeResponse';
+    var vinDecodeURL = vinDecodeBaseURL + vinNumber +vinDecodeResponseFormat + vinDecodeCallback;
+    function addScriptVinCall(vinDecodeURL) {
+        var script = document.createElement('script');
+        script.type= "text/javascript";
+        script.src = vinDecodeURL;
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+    addScriptVinCall();
+}
+
 function runVinDecoder(){
     var vinDecodeButton = document.getElementById('vin-decode-button');
     var vinInputField = document.getElementById('vin-input-field');
     var vinNumber = vinInputField.value;
     vinDecodeButton().addEventListener('click', function(){
         if (vinNumber.length == 17){
-            vinDecode(vinNumber);
+            vinDecodeMakeRequest(vinNumber);
         }
         else{
             alert('Check the length of the VIN, must be 17 characters long.');
@@ -19,29 +33,11 @@ function runVinDecoder(){
     });
 }
 
-function processVinDecodeResponse();
+function processVinDecodeResponse(obj){
+    console.log(obj);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    vinDecodeCall(vinNumber){
-        //var xhttp = new XMLHttpRequest();
-        var vinDecodeBaseURL = 'https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/';
-        var vinDecodeResponseFormat = '?format=json';
-        var vinDecodeCallback = '&jsoncallback=processVinDecodeResponse';
-        var vinDecodeURL = vinDecodeBaseURL + vinNumber +vinDecodeResponseFormat + vinDecodeCallback;
-        function addScriptVinCall(vinDecodeURL) {
-            var script = document.createElement('script');
-            script.type="text/javascript";
-            script.src = url;
-            document.getElementsByTagName('head')[0].appendChild(script);
-        }
-        addScriptVinCall();
-    }
-    
-    var vinDecodeButton = document.getElementById('vin-decode-button');
-    var vinInputField = document.getElementById('vin-input-field');
-    var vinNumber = vinInputField.value;
-    vinDecodeButton().addEventListener('click', vinDecode(vinNumber));
-    
     setAllInputCleaves();
     runVinDecoder();
 });
